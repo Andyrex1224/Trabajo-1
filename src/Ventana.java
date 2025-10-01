475605e7871df35f50559917751c75a5e13cb904
Laboratorio <laboratorio@udla-ec.int>
2025-10-01 17:14:57 -0500

    Trabajo 1

---
+++ src/Ventana.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ventana {
+    private JPanel principal;
+    private JTextField txtCodigo;
+    private JTextField txtTitulo;
+    private JTextArea txtArea;
+    private JLabel txtTitulo1;
+    private JLabel txtMensaje1;
+    private JButton butincertar;
+    private JTextArea txtListar;
+    private JLabel Codigo;
+
+    Pila pila= new Pila();
+
+
+    public Ventana() {
+        butincertar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int codigo=Integer.parseInt(txtCodigo.getText());
+                String titulo= txtTitulo.getText();
+                String mensaje= txtArea.getText();
+
+                Publicacion post= new Publicacion(codigo , titulo , mensaje);
+
+                pila.push(post);
+
+                txtListar.setText(pila.toString());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ventana");
+        frame.setContentPane(new Ventana().principal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
